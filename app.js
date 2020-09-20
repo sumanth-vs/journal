baec4dc8101d0929c4a5642780e3252a1e1ba949
Sumanth VS <vs.sumanth2k@gmail.com>
2020-09-21 00:41:25 +0530

    project files

---
+++ app.js
@@ -0,0 +1,84 @@
+// BoilerPlate
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Starting Content
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+let blogPosts = [];
+
+// GET
+app.get("/", (req, res) => {
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    blogPosts: blogPosts,
+  });
+});
+
+app.get("/:action", (req, res) => {
+  let action = req.params.action;
+  switch (action) {
+    case "/":
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        blogPosts: blogPosts,
+      });
+      break;
+
+    case "about":
+      res.render("about", { aboutContent: aboutContent });
+      break;
+
+    case "contact":
+      res.render("contact", { contactContent: contactContent });
+      break;
+
+    case "compose":
+      res.render("compose");
+      break;
+
+    default:
+      break;
+  }
+});
+
+app.get("/posts/:post", (req, res) => {
+  //convert req urls to lower-kebab-case
+  let post = _.kebabCase(_.lowerCase(_.trim(req.params.post)));
+
+  blogPosts.forEach((e) => {
+    // convert page urls to lower-kebab-case
+    let lowerCaseTitle = _.kebabCase(_.lowerCase(_.trim(e.title)));
+    if (lowerCaseTitle === post) {
+      res.render("post", { blogPosts: e });
+    }
+  });
+});
+
+//POST
+app.post("/compose", (req, res) => {
+  let newBlog = {
+    title: req.body.newBlogTitle,
+    blog: req.body.newBlog,
+  };
+  blogPosts.push(newBlog);
+  res.redirect("/");
+});
+
+//SERVER LISTEN
+app.listen(3000, function () {
+  console.log("Server started on PORT 3000");
+});
